4d21418f5c6c1ed7b6a10fce0944f6e95b72687b
Angel1613 <angela.m@onet.eu>
2018-10-15 17:16:31 +0200

    Dodanie i implementacja klasy TXTStorageFactory.

---
+++ src/car/storage/TXTStorageFactory.java
@@ -0,0 +1,12 @@
+package car.storage;
+
+public class TXTStorageFactory implements IStorageFactory {
+
+    public String getModelName() {
+        return null;
+    }
+
+    public String getBrandName() {
+        return null;
+    }
+}
