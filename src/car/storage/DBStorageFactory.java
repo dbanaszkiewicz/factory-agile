9bc00580a911b5fb0e8d1235fb59181a052b6a0a
Valpor <37022061+Valpor@users.noreply.github.com>
2018-10-15 18:54:33 +0200

    DBStorageFactory singleton

--- src/car/storage/DBStorageFactory.java
+++ src/car/storage/DBStorageFactory.java
@@ -3,6 +3,13 @@ package car.storage;
 import car.IStorageFactory;
 
 public class DBStorageFactory implements IStorageFactory {
+    private static DBStorageFactory instance = new DBStorageFactory();
+
+    private DBStorageFactory(){}
+
+    public static DBStorageFactory getInstance(){
+        return instance;
+    }
 
     public String getModelName(){
         return null;
