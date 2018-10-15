6620164009302fadd8a6285dc0c8e238a593680c
Angel1613 <angela.m@onet.eu>
2018-10-15 18:57:06 +0200

    Zrobienie singletona w TXTStorageFactory.

--- src/car/storage/TXTStorageFactory.java
+++ src/car/storage/TXTStorageFactory.java
@@ -3,6 +3,13 @@ package car.storage;
 import car.IStorageFactory;
 
 public class TXTStorageFactory implements IStorageFactory {
+    private static TXTStorageFactory instance = new TXTStorageFactory();
+
+    private TXTStorageFactory(){}
+
+    public static TXTStorageFactory getInstance(){
+        return instance;
+    }
 
     public String getModelName() {
         return null;
