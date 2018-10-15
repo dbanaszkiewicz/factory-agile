bf7632b5a0b6eba36ea75b08459c2a6029cbe424
Valpor <37022061+Valpor@users.noreply.github.com>
2018-10-15 17:47:53 +0200

    Dodanie DBStorageFactory

---
+++ src/car/storage/DBStorageFactory.java
@@ -0,0 +1,14 @@
+package car.storage;
+
+import car.IStorageFactory;
+
+public class DBStorageFactory implements IStorageFactory {
+
+    public String getModelName(){
+        return null;
+    }
+
+    public String getBrandName(){
+        return null;
+    }
+}
